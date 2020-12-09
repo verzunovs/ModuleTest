bde1bfd6f9cf9f0a5a7337c004ad47efb1edf633
Serhii Verzunov <shvabroid1221@gmail.com>
2020-12-09 03:06:20 +0200

    Added StyleCop, remove rule "SA1600".

--- Program.cs
+++ Program.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HomeWorkForModuleTest
+﻿namespace HomeWorkForModuleTest
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
         }
     }
