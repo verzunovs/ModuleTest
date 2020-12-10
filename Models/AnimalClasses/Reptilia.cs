2e9253213bf5f187123cc132f2fd569f70b1f3ff
Serhii Verzunov <shvabroid1221@gmail.com>
2020-12-10 18:09:22 +0200

    Make folder distribution.

--- Models/AnimalClasses/Reptilia.cs
+++ Models/AnimalClasses/Reptilia.cs
@@ -2,9 +2,9 @@
 {
     using HomeWorkForModuleTest.Enums;
 
-    public abstract class Reptile : Animal
+    public abstract class Reptilia : Animal
     {
-        public Reptile(double weight, double amountOfFoodRequiredPerDay, int age, Sex sex)
+        public Reptilia(double weight, double amountOfFoodRequiredPerDay, int age, Sex sex)
             : base(weight, amountOfFoodRequiredPerDay, age, sex)
         {
         }
