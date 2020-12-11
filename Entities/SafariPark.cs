3166407b058965bc8863827fc95a92373b7900af
Serhii Verzunov <shvabroid1221@gmail.com>
2020-12-11 13:26:05 +0200

    Added comparer for animals

---
+++ Entities/SafariPark.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeWorkForModuleTest.Entities
+{
+    public class SafariPark
+    {
+        public Animal[] Animals { get; set; }
+
+        public int Price { get; set; }
+    }
+}
---
+++ Helper/AnimalComparer.cs
@@ -0,0 +1,25 @@
+﻿namespace HomeWorkForModuleTest.Helper
+{
+    using System.Collections;
+
+    public class AnimalComparer : IComparer
+    {
+        public int Compare(object first, object second)
+        {
+            var x = first as Animal;
+            var y = second as Animal;
+            if (x.Weight > y.Weight)
+            {
+                return 1;
+            }
+            else if (x.Weight < y.Weight)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
